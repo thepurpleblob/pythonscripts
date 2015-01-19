a1c0bac9718739e708b1a587c3723ebb198b9ca9
Howard Miller <howardsmiller@gmail.com>
2015-01-19 16:11:53 +0000

    basic aaw data framework

--- aaw.py
+++ aaw.py
@@ -1,9 +1,14 @@
+import sys
 import pymysql
 import pprint
 import aawlib
 
 # instantiate PrettyPrint for debugging
 pp = pprint.PrettyPrinter(indent=4);
+
+# holder for current data hierarchy
+class ttclass:
+    pass
 
 # connect to aaw db
 conn = pymysql.connect(host='localhost', user='root', db='aaw');
@@ -12,10 +17,44 @@ conn = pymysql.connect(host='localhost', user='root', db='aaw');
 courses = aawlib.getcourses(conn)
 
 # iterate over courses
+tt = ttclass()
 for course in courses:
-    courseid = course[0]
-    fullname = course[1]
-    shortname = course[2]
-    print("Processing course " + fullname)
+    tt.courseid = course[0]
+    tt.fullname = course[1]
+    tt.shortname = course[2]
+    print("Processing course " + tt.fullname)
+
+    # get writings in current course
+    writings = aawlib.getwritings(conn, tt.courseid)
+
+    # iterate over writings
+    for writing in writings:
+        tt.writingid = writing[0]
+        tt.name = writing[2]
+        tt.intro = writing[3]
+        print("    Writing activity " + tt.name);
+
+        # get tasks in current writing
+        tasks = aawlib.gettasks(conn, tt.writingid)
+
+        # iterate over tasks
+        for task in tasks:
+            tt.taskid = task[0]
+            tt.tasktype = task[2]
+            tt.tasktypeid = task[3]
+            tt.difficulty = task[4]
+            print( "        Task " + tt.tasktype + " difficulty " + str(tt.difficulty))
+
+            # handler for task type
+            if tt.tasktype == 'multiplechoice':
+                aawlib.multiplechoice(conn, tt)
+            elif tt.tasktype == 'wordclick':
+                aawlib.wordclick(conn, tt)
+            elif tt.tasktype == 'freetext':
+                aawlib.freetext(conn, tt)
+            elif tt.tasktype == 'selectcat':
+                aawlib.freetext(conn, tt)
+            else:
+                print( "            Tasktype not recognised!")
 
     
--- aawlib.py
+++ aawlib.py
@@ -11,3 +11,47 @@ def getcourses(conn):
     cur.close()
     return courses
 
+# 
+# get the list of writing activities within a course
+#
+def getwritings(conn, courseid):
+    cur = conn.cursor()
+    sql = 'SELECT * FROM mdl_writing WHERE course=' + str(courseid)
+    cur.execute(sql)
+    writings = cur.fetchall()
+    cur.close()
+    return writings
+
+# 
+# get the list of tasks within a writing activity
+#
+def gettasks(conn, writingid):
+    cur = conn.cursor()
+    sql = 'SELECT * FROM mdl_writingtask WHERE writingid=' + str(writingid)
+    cur.execute(sql)
+    tasks = cur.fetchall()
+    cur.close()
+    return tasks
+
+#
+# Handle multiplechoice
+#
+def multiplechoice(conn, tt):
+    return
+
+#
+# Handle wordclick
+#
+def wordclick(conn, tt):
+    return
+
+#
+# Handle freetext
+#
+def freetext(conn, tt):
+    return
+#
+# Handle selectcat
+#
+def multiplechoice(conn, tt):
+    return
